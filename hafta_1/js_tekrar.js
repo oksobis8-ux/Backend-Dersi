b54ba6f6030fa4d22f4bbc6800a91fee595182ce
oksobis8-ux <oksobis8@gmail.com>
2026-04-26 15:29:12 +0300

    javascrpit ve typescript kodları tekrar edildi

--- hafta_1/js_tekrar.js
+++ hafta_1/js_tekrar.js
@@ -1,2 +1,89 @@
-console.log ("js başlangıç");
-console.log ("js başlangıç2");
+// let const var
+
+let ad = "Mehmet"; //string
+const yil = 2000; //number
+let bilgi = true; //bool
+
+let isimler = ["baran","nisa","goktug"] //array
+
+    let ogrenci = {
+        ad : "ahmet",
+        okul : "çakir"
+    }//object
+
+    //koşullar
+    if (2026-yil < 22){
+        console.log("ogrenci kart")
+    }else if (2026-yil >65){
+        console.log("65 yaş üstü kart")
+     } else {
+        console.log ("tam kart")
+     }
+
+     //döngüler
+
+     for (let i=0;i<isimler.length;i++){
+        console.log("isimler[i]")
+     }
+ let sayac = 0
+     while(true){
+        console.log(isimler[sayac])
+     
+     if(sayac >= isimler[sayac]){
+        break
+     }
+     sayac++}
+    //en az bir kere çalıştır koşul sağlanmasa da
+     do{
+        console.log("sayac degeri : ",sayac, "bir kere çalistir")
+     }while (sayac < 3);
+     
+
+     // foreach
+
+     for (isim of isimler){
+        console.log("isim :",isim)
+     };
+     
+     //Fonksiyonlar
+//parametreli dönüşümlü
+     function veri_uret(a,b){
+        return a+b
+     };
+     let sonuc = veri_uret (10.15);
+     console.log(sonuc)
+     function param_donussuz(a){
+        console.log("donussuz")
+     };
+     param_donussuz(15)
+
+     //ES6 sonrası
+     //arrow function
+     const carp = (a,b) => {
+        return a*b;
+     };
+     console.log("carp(5):",carp(5))
+
+    
+
+    //class - OOP
+
+    class Kullanici{
+
+        constructor(kullanici_adi,sifre){
+            this.kullanici_adi = kullanici_adi
+            this.sifre = sifre
+        }
+    
+    bilgi_yaz(){
+        console.log("ad : ", this.kullanici_adi)
+        console.log("sifre :", this.sifre)
+    }
+    static bilgi(){
+        console.log("this olmadan")
+    }
+}
+Kullanici.bilgi()
+const kullanici1 = new Kullanici("osman","osman1234!");
+kullanici1.bilgi_yaz()
+
---
+++ hafta_1/typecript_tekrar.ts
@@ -0,0 +1,42 @@
+let yas : number = 10;
+let isim : string ="backend dersi";
+
+// ts dosyalarını çalıştırmak için 
+// npx ts-node ./hafta_1/DOSYANIN_İSMİ.ts
+
+let hazir_mi : boolean = true
+
+// Interface -> arayüzler
+interface Iogrenci{
+    isim:string;
+    soyisim:string;
+    yas:number;
+    mezun:boolean;
+}
+
+let ogrenci : Iogrenci = {
+    isim : "Mehmet",
+    soyisim : "taka",
+    yas : 16,
+    mezun : false
+}
+
+class Araba{
+    public marka : string;
+    public hiz : number;
+    
+    constructor (marka:string,hiz:number){
+        this.marka = marka
+        this.hiz = hiz
+    }
+    vites_arttir(vites_numarasi:number){
+        const artacak_hiz : number = vites_numarasi * 15;
+        this.hiz = this.hiz + artacak_hiz;
+        return this.hiz;
+
+    }
+}
+
+const araba1 = new Araba("BMW",10);
+let son_hiz = araba1.vites_arttir(4); 
+console.log("son hız:",son_hiz)
